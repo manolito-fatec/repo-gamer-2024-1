78bc69be4c7ddc9ec445b712869314ea568b3ce2
André Wakugawa <andre.wakugawa@outlook.com>
2024-08-21 22:01:17 -0300

    🎉🎉🎉 begin spring project 🎉🎉🎉

---
+++ src/main/java/com/example/demo/Ito1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ito1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ito1Application.class, args);
+	}
+
+}
