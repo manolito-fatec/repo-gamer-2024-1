2dddcfd74930bfd2249d68fd301571c06479c6f7
André Wakugawa <117671087+AndreWakugawa@users.noreply.github.com>
2024-09-17 14:37:33 -0300

    ♻️refactor: remove tracker public arg + add interval method

--- src/main/java/com/example/geoIot/service/device/DeviceTrackerService.java
+++ src/main/java/com/example/geoIot/service/device/DeviceTrackerService.java
@@ -1,10 +1,14 @@
 package com.example.geoIot.service.device;
 
 import com.example.geoIot.entity.DeviceTracker;
+import com.example.geoIot.entity.dto.DeviceTrackerDto;
+import com.example.geoIot.entity.dto.DeviceTrackerPeriodRequestDto;
 
 import java.util.List;
 
 
 public interface DeviceTrackerService {
-    public void saveDeviceTracker(List<DeviceTracker> pDeviceTracker);
+    void saveDeviceTracker(List<DeviceTracker> pDeviceTracker);
+
+    List<DeviceTrackerDto> getDeviceTrackerByDateInterval(DeviceTrackerPeriodRequestDto requestDto);
 }
