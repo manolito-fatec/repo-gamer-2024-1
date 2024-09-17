d3b0c617f31735ac72d434d59b77755a8f3adf1e
André Wakugawa <117671087+AndreWakugawa@users.noreply.github.com>
2024-09-17 14:39:46 -0300

    ✨feat: get person tracker by interval repo method

--- src/main/java/com/example/geoIot/repository/DeviceTrackerRepository.java
+++ src/main/java/com/example/geoIot/repository/DeviceTrackerRepository.java
@@ -3,5 +3,11 @@ package com.example.geoIot.repository;
 import com.example.geoIot.entity.DeviceTracker;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface DeviceTrackerRepository extends JpaRepository<DeviceTracker, Long>{
+
+    public Optional<List<DeviceTracker>> findByPersonDeviceTrackerIdAndCreatedAtDeviceTrackerBetween(Long personId, LocalDateTime init, LocalDateTime end);
 }
