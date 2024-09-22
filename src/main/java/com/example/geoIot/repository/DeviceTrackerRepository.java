9152223a88bd979dc8d42c39c627af1902d24519
AndreWakugawa <andre.wakugawa@outlook.com>
2024-09-22 14:54:25 -0300

    🩹fix: change tracker repo list method to pageable

--- src/main/java/com/example/geoIot/repository/DeviceTrackerRepository.java
+++ src/main/java/com/example/geoIot/repository/DeviceTrackerRepository.java
@@ -1,13 +1,13 @@
 package com.example.geoIot.repository;
 
 import com.example.geoIot.entity.DeviceTracker;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
 
 public interface DeviceTrackerRepository extends JpaRepository<DeviceTracker, Long>{
 
-    public Optional<List<DeviceTracker>> findByPersonDeviceTrackerIdPersonAndCreatedAtDeviceTrackerBetween(Long personId, LocalDateTime init, LocalDateTime end);
+    public Page<DeviceTracker> findByPersonDeviceTrackerIdPersonAndCreatedAtDeviceTrackerBetweenOrderByCreatedAtDeviceTracker(Long personId, LocalDateTime init, LocalDateTime end, Pageable pageable);
 }
