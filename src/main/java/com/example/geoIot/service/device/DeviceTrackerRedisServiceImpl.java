cfd737af6240d30d2520229b7d2a3c491ed456bd
pauloarantesmahado <pauloantoniomachado@hotmail.com>
2024-09-30 18:33:23 -0300

    :adhesive_bandage:fix: add method onEvent() and added in interface and his body

--- src/main/java/com/example/geoIot/service/device/DeviceTrackerRedisServiceImpl.java
+++ src/main/java/com/example/geoIot/service/device/DeviceTrackerRedisServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.geoIot.service.device;
 
+import com.example.geoIot.component.personList.StartupPersonListComponent;
 import com.example.geoIot.entity.Person;
 import com.example.geoIot.exception.EmptyDataListInRedisException;
 import com.example.geoIot.entity.DeviceTracker;
@@ -8,6 +9,7 @@ import com.example.geoIot.entity.dto.DeviceTrackerRedisDto;
 import com.example.geoIot.exception.PersonNotFoundException;
 import com.example.geoIot.repository.DeviceTrackerRedisRepository;
 import com.example.geoIot.service.person.PersonService;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -16,16 +18,15 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
 @EnableScheduling
-public class DeviceTrackerRedisServiceImpl implements DeviceTrackerRedisService {
+public class DeviceTrackerRedisServiceImpl implements DeviceTrackerRedisService{
 
-    private final long MINUTES = 1000 * 60;
+    private final long MINUTES = 1000 * 60 *5;
 
     @Autowired
     private DeviceTrackerRedisRepository deviceTrackerRedisRepository;
@@ -38,23 +39,27 @@ public class DeviceTrackerRedisServiceImpl implements DeviceTrackerRedisService 
 
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS");
 
-    private  Set<Person> personSet = new HashSet<>();
+    @Getter
+    private  Set<Person> personSet;
+
 
     @Override
     public void saveDataInCache(List<DeviceTrackerRedisDto> pDeviceTrackerRedis) {
         if (pDeviceTrackerRedis.isEmpty()) {
             throw new EmptyDataListInRedisException();
         }
+        if(this.personSet == null){
+            this.personSet = StartupPersonListComponent.getPersons();
+        }
         List<DeviceTrackerRedis> listConverted = this.convertToDeviceTrackerListRedis(pDeviceTrackerRedis,personSet);
         deviceTrackerRedisRepository.saveAll(listConverted);
+        this.synchronizeDataBase();
     }
 
     @Override
     @Scheduled(fixedDelay = MINUTES)
     public void synchronizeDataBase() {
-        this.personSet = personService.getAllPersons();
-        List<DeviceTrackerRedis> deviceTrackerRedisList = this.findAllInRedis();
-
+       List<DeviceTrackerRedis> deviceTrackerRedisList = this.findAllInRedis();
        if(!(deviceTrackerRedisList == null) || !(deviceTrackerRedisList.isEmpty())){
            List<DeviceTracker> deviceTrackerList = this.convertToDeviceTrackerList(deviceTrackerRedisList);
            this.deviceTrackerService.saveDeviceTracker(deviceTrackerList);
@@ -104,4 +109,12 @@ public class DeviceTrackerRedisServiceImpl implements DeviceTrackerRedisService 
                 })
                 .collect(Collectors.toList());
     }
+
+    @Override
+    public void onEvent() {
+        this.personSet = (personService.getAllPersons() != null)
+                ? personService.getAllPersons()
+                : StartupPersonListComponent.getPersons();
+    }
+
 }
