6de611e575475ca555c68ac53956e3cb30547544
ocalderan <otaviocalderanadm@gmail.com>
2024-10-03 19:52:27 -0300

    💡 (chore): Remove unnecessary comments

--- src/main/java/com/example/geoIot/exception/ControllerAdvice/GlobalExceptionHandler.java
+++ src/main/java/com/example/geoIot/exception/ControllerAdvice/GlobalExceptionHandler.java
@@ -12,37 +12,37 @@ public class GlobalExceptionHandler {
 
     Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-    @ExceptionHandler(InvalidRequestException.class) //400
+    @ExceptionHandler(InvalidRequestException.class)
     public ResponseEntity<String> handleInvalidRequestException(InvalidRequestException ex) {
         logger.error(ex.getMessage());
         return new ResponseEntity<>("Solicitação inválida. Verifique os dados recebidos", HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(InvalidInputException.class) //400
+    @ExceptionHandler(InvalidInputException.class)
     public ResponseEntity<String> handleInvalidInputException(InvalidInputException ex) {
         logger.error(ex.getMessage());
         return new ResponseEntity<>("Solicitação inválida. Verifique os dados enviados", HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(NoDataFoundException.class) //204
+    @ExceptionHandler(NoDataFoundException.class)
     public ResponseEntity<String> handleNoDataFoundException(NoDataFoundException ex) {
         logger.error(ex.getMessage());
         return new ResponseEntity<>("Nenhum conteúdo disponível", HttpStatus.NO_CONTENT);
     }
 
-    @ExceptionHandler(ResourceNotFoundException.class) //404
+    @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
         logger.error(ex.getMessage());
         return new ResponseEntity<>("O recurso solicitado não foi encontrado", HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(RequestTimeoutException.class) //408
+    @ExceptionHandler(RequestTimeoutException.class)
     public ResponseEntity<String> handleRequestTimeoutException(RequestTimeoutException ex) {
         logger.error(ex.getMessage());
         return new ResponseEntity<>("A requisição demorou muito para ser processada. Tente novamente", HttpStatus.REQUEST_TIMEOUT);
     }
 
-    @ExceptionHandler(InternalServerErrorException.class) //500
+    @ExceptionHandler(InternalServerErrorException.class)
     public ResponseEntity<String> handleInternalServerErrorException(InternalServerErrorException ex) {
         logger.error(ex.getMessage());
         return new ResponseEntity<>("Erro interno no servidor. Tente novamente mais tarde ou acione o suporte", HttpStatus.INTERNAL_SERVER_ERROR);
