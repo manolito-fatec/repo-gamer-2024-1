d73d449cc3978b2228b2f80f92c53bbf9815d963
AndreWakugawa <andre.wakugawa@outlook.com>
2024-10-03 23:14:46 -0300

    ✨feat: Location Entity mapping

---
+++ src/main/java/com/example/geoIot/entity/Location.java
@@ -0,0 +1,25 @@
+package com.example.geoIot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import oracle.spatial.geometry.JGeometry;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="location")
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="id_person")
+    private Long idLocation;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "poly")
+    private JGeometry poly;
+}
