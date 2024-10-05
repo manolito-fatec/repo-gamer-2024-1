dc10c3035ca8333ab440f55df7c799e3e249409d
AndreWakugawa <andre.wakugawa@outlook.com>
2024-10-04 21:15:00 -0300

    ✨feat: save and get Location service layer logic

--- src/main/java/com/example/geoIot/service/location/LocationServiceImpl.java
+++ src/main/java/com/example/geoIot/service/location/LocationServiceImpl.java
@@ -1,9 +1,14 @@
 package com.example.geoIot.service.location;
 
 import com.example.geoIot.entity.Location;
+import com.example.geoIot.entity.dto.CoordinateDto;
+
+import com.example.geoIot.entity.dto.LocationDto;
+import com.example.geoIot.entity.dto.PolygonSaveDto;
 import com.example.geoIot.repository.LocationRepository;
-import oracle.spatial.geometry.JGeometry;
+import com.example.geoIot.util.CoordinateValidator;
 import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,31 +24,47 @@ public class LocationServiceImpl implements LocationService {
     @Autowired
     private LocationRepository locationRepository;
 
-    private final int DIMENSIONS = 2;
-    private final int SRID = 8307;
+    private CoordinateValidator coordinateValidator;
 
     @Transactional
     @Override
-    public Location getLocation(Long id) {
+    public LocationDto getLocation(Long id) {
         Optional<Location> location = locationRepository.findById(id);
         if (location.isEmpty()) {
             throw new NoSuchElementException("No such location");
         }
-        return location.get();
+        return LocationDto.builder()
+                .idLocation(location.get().getIdLocation())
+                .name(location.get().getName())
+                .polygon(location.get().getPolygon())
+                .build();
     }
 
     @Transactional
     @Override
-    public Location saveLocation(String name, double[][] polyCoords) {
+    public LocationDto saveLocation(PolygonSaveDto polygonSaveDto) {
         Location location = new Location();
-        location.setName(name);
+        location.setName(polygonSaveDto.getName());
 
         List<Coordinate> coords = new ArrayList<>();
-        coords.add
+        for (CoordinateDto dotCoords : polygonSaveDto.getCoordinates()) {
+            coordinateValidator.validateCoordinate(
+                    dotCoords.getLongitude(),
+                    dotCoords.getLatitude()
+            );
 
+            coords.add(new Coordinate(dotCoords.getLongitude(), dotCoords.getLatitude()));
+        }
+        GeometryFactory geometry = new GeometryFactory();
 
+        location.setPolygon(geometry.createPolygon((coords.toArray(new Coordinate[0]))));
 
-        return locationRepository.save(location);
+        Location savedLocation = locationRepository.save(location);
+        return LocationDto.builder()
+                .idLocation(savedLocation.getIdLocation())
+                .name(savedLocation.getName())
+                .polygon(savedLocation.getPolygon())
+                .build();
 
     }
 }
