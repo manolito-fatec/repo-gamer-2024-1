f4b35a9a0b60de66e9690175a0fd734cffae1ad2
pauloarantesmahado <pauloantoniomachado@hotmail.com>
2024-10-13 17:24:38 -0300

    :construction:feat: add searchHistoryByDateIntervel

--- src/main/java/com/example/geoIot/service/device/DeviceTrackerService.java
+++ src/main/java/com/example/geoIot/service/device/DeviceTrackerService.java
@@ -3,6 +3,7 @@ package com.example.geoIot.service.device;
 import com.example.geoIot.entity.DeviceTracker;
 import com.example.geoIot.entity.dto.DeviceTrackerDto;
 import com.example.geoIot.entity.dto.DeviceTrackerPeriodRequestDto;
+import com.example.geoIot.entity.dto.history.HistoryDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -13,4 +14,6 @@ public interface DeviceTrackerService {
     void saveDeviceTracker(List<DeviceTracker> pDeviceTracker);
 
     Page<DeviceTrackerDto> getDeviceTrackerByDateInterval(DeviceTrackerPeriodRequestDto requestDto, Pageable pageable);
+
+    Page<HistoryDto> searchHistoryByDateInterval(DeviceTrackerPeriodRequestDto requestDto, Pageable pageable);
 }
