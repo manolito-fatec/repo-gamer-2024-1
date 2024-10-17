e3486de067687ed62c84b2b00cbf54cdfd0807a8
AndreWakugawa <andre.wakugawa@outlook.com>
2024-10-17 17:46:57 -0300

    🚨fix: add coordinateValidator injection

--- src/main/java/com/example/geoIot/service/location/LocationServiceImpl.java
+++ src/main/java/com/example/geoIot/service/location/LocationServiceImpl.java
@@ -26,6 +26,7 @@ public class LocationServiceImpl implements LocationService {
     @Autowired
     private LocationRepository locationRepository;
 
+    @Autowired
     private CoordinateValidator coordinateValidator;
 
     @Transactional
